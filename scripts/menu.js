ae579a8d86df19c4fdecd2ed5bcf63ca89404cb2
Simon Fermor <simon.fermor@gmail.com>
2016-04-11 09:24:06 -0500

    Adding page for session 9.

--- scripts/menu.js
+++ scripts/menu.js
@@ -16,6 +16,7 @@ header += '				<li><a href="/coding-studio/views/pages/session_04.html">Session 
 header += '				<li><a href="/coding-studio/views/pages/session_05.html">Session 5</a></li>';
 header += '				<li><a href="/coding-studio/views/pages/session_07.html">Session 7</a></li>';
 header += '				<li><a href="/coding-studio/views/pages/session_08.html">Session 8</a></li>';
+header += '				<li><a href="/coding-studio/views/pages/session_09.html">Session 9</a></li>';
 header += '			</ul>';
 header += '		</li><li>&nbsp;|&nbsp;</li>';
 header += '		<li><a href="/coding-studio/views/pages/resources.html">Resources</a></li><li>&nbsp;|&nbsp;</li>';
@@ -29,17 +30,17 @@ $('#menu').html(header);
 $(function(){
 
     $("ul.dropdown li").hover(function(){
-    
+
         $(this).addClass("hover");
         $('ul:first',this).css('visibility', 'visible');
-    
+
     }, function(){
-    
+
         $(this).removeClass("hover");
         $('ul:first',this).css('visibility', 'hidden');
-    
+
     });
-    
+
     $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
 
 });
